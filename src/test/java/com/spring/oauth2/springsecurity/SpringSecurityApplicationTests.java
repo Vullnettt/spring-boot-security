78417511ad8648868932c994ff812a3545788510
Vullnettt <vullnetv389@gmail.com>
2023-06-15 01:34:44 +0200

    Initial Project

---
+++ src/test/java/com/spring/oauth2/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.oauth2.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
