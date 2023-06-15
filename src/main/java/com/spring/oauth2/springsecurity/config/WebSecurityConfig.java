3842e29d854f3aba9513907009b1d72074db9107
Vullnettt <vullnetv389@gmail.com>
2023-06-15 13:30:31 +0200

    implement services in websecConfig

--- src/main/java/com/spring/oauth2/springsecurity/config/WebSecurityConfig.java
+++ src/main/java/com/spring/oauth2/springsecurity/config/WebSecurityConfig.java
@@ -1,6 +1,10 @@
 package com.spring.oauth2.springsecurity.config;
 
+import com.spring.oauth2.springsecurity.oauth2.security.CustomOAuth2UserDetailService;
+import com.spring.oauth2.springsecurity.oauth2.security.handler.CustomOAAuth2FailureHandler;
+import com.spring.oauth2.springsecurity.oauth2.security.handler.CustomOAAuth2SuccessHandler;
 import com.spring.oauth2.springsecurity.user.UserDetailsServiceCustom;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,6 +21,15 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Autowired
+    private CustomOAuth2UserDetailService customOAuth2UserDetailService;
+
+    @Autowired
+    private CustomOAAuth2FailureHandler customOAAuth2FailureHandler;
+
+    @Autowired
+    private CustomOAAuth2SuccessHandler customOAAuth2SuccessHandler;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
@@ -65,6 +78,14 @@ public class WebSecurityConfig {
                 .oauth2Login()
                 .loginPage("/login")
                 .defaultSuccessUrl("/home/index", true)
+                .userInfoEndpoint()
+                .userService(customOAuth2UserDetailService)
+                .and()
+                .successHandler(customOAAuth2SuccessHandler)
+                .failureHandler(customOAAuth2FailureHandler)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
         ;
 
         return http.build();
